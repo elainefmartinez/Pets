3594d638a33366b4d6fc81900449fb9fb5d5a25f
Louis Antolin <ragnaman.exe@gmail.com>
2023-10-12 02:39:02 -0700

    started to debug so it builds

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import { setContext } from '@apollo/client/link/context';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import SingleThought from './pages/SingleThought';
+import SinglePost from './pages/SinglePost';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -52,7 +52,7 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
-              {/* <Route 
+              <Route 
                 path="/"
                 element={<Home />}
               />
@@ -75,8 +75,8 @@ function App() {
               />
               <Route 
                 path="/thoughts/:thoughtId" 
-                element={<SingleThought />}
-              /> */}
+                element={<SinglePost />}
+              />
             </Routes>
           </div>
           <Footer />
